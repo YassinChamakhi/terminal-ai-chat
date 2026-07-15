b1f57ab434d7dbdc2ee02bd70ba7aef53a8cca78
Yassine <190398815+YassinChamakhi@users.noreply.github.com>
2026-07-15 01:49:00 +0100

    chore: replace deprecated sqlite.exec with sqlite.run

--- db/index.ts
+++ db/index.ts
@@ -3,11 +3,8 @@ import { Database } from "bun:sqlite";
 import * as schema from "./schema";
 
 const sqlite = new Database("chat.db");
-sqlite.exec("PRAGMA journal_mode = WAL;");
-sqlite.exec("PRAGMA foreign_keys = ON;");
-sqlite.exec("PRAGMA busy_timeout = 5000;");
-
-const mode = sqlite.query("PRAGMA journal_mode;").get();
-console.log("Journal mode:", mode);
+sqlite.run("PRAGMA journal_mode = WAL;");
+sqlite.run("PRAGMA foreign_keys = ON;");
+sqlite.run("PRAGMA busy_timeout = 5000;");
 
 export const db = drizzle(sqlite, { schema });
